00f98fee5cd4aa3ca99f413dec52b5ae01137427
bjacobi <bjacobi@adaptiverx.com>
2016-08-11 18:18:51 -0500

    multi-tenant r&d

--- grails-app/controllers/jamnApps/scheduler/AdminInterceptor.groovy
+++ grails-app/controllers/jamnApps/scheduler/AdminInterceptor.groovy
@@ -4,6 +4,13 @@ package jamnApps.scheduler
 class AdminInterceptor {
 
     boolean before() {
+        println "request.serverName: " + request.serverName
+        def clientName = request.serverName
+        if (clientName.contains(".")) {
+            clientName = clientName.substring(0, clientName.indexOf("."))
+            //request.currentClient = Client.findByClientName(clientName) // e.g.
+            println "clientName: " + clientName
+        }
         if (!session.user?.isAdmin){
             redirect(controller:'access', action:'login', params: [caller:'/admin'])
             return false
